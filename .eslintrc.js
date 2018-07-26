8da4518a4f5b99371733087b4f90661d9e3928b8
Kenrick <kenrick95@gmail.com>
2018-07-26 22:08:03 +0800

    Setup configs

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2015
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+}
